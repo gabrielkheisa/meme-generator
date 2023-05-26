b5e9f79f525e5ab04f5cf9477eb2787d6350ad67
Gabriel Kheisa <63294471+gabrielkheisa@users.noreply.github.com>
2023-05-26 10:41:50 +0700

    Update check.php

--- ronaldo/check.php
+++ ronaldo/check.php
@@ -24,7 +24,7 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
     if($row["status"] == "1"){
-        echo "<a href=\"$vid\">Click here</a>";
+        echo "<a href=\"output/$vid\">Click here</a>";
     }
     else {
       echo "Processing";
